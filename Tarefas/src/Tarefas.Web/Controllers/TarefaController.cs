33d2e8d015616ff09e2bf871204a070cd8cd47e8
Tales Félix <tales.felix.10@gmail.com>
2023-01-25 18:40:53 -0300

    first commit

---
+++ Tarefas/src/Tarefas.Web/Controllers/TarefaController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarefas.Web.Controllers 
+{
+    public class TarefaController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tarefas/src/Tarefas.Web/Models/TarefaViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+
+namespace Tarefas.Web.Models 
+{
+    public class TarefaViewModel 
+    {
+        [DisplayName("Título")]
+        public string Titulo {get;set;}
+        
+        [DisplayName("Descrição")]
+        public string Descricao {get;set;}
+    }
+}
